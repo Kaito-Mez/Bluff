7d775636df21f6d4f67051fcc204f588d84b5270
Kaito-Mez <jameskm721@gmail.com>
2024-02-02 22:54:28 +1100

    Created Player Unit Tests

---
+++ frontend/test/unit/player_test.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:frontend/game/models/player.dart';
+
+import '../data/dice_test_data.dart';
+import '../data/player_test_data.dart';
+
+void main() async {
+  test('Rolling dice should set their values', () {
+    List<int> expectedInitialRole = List.of(<int>[-1, -1, -1, -1, -1, -1]);
+    Player player = getPlayer();
+
+    expect(player.currentRoll, expectedInitialRole);
+
+    List<int> roll = player.rollDice();
+
+    expect(player.currentRoll, isNot(expectedInitialRole));
+    expect(player.currentRoll, roll);
+  });
+
+  group("Modify Hand Size:", () {
+    test('Adding dice should increase hand size', () {
+      int expectedHandSize = 6;
+      Player player = getPlayer();
+      expect(player.numDice(), expectedHandSize);
+
+      player.addDice(2, getDie());
+      expectedHandSize = 8;
+
+      expect(player.numDice(), expectedHandSize);
+    });
+
+    test('Discarding dice should decrease hand size', () {
+      int expectedHandSize = 6;
+      Player player = getPlayer();
+      expect(player.numDice(), expectedHandSize);
+
+      player.discardDice(4);
+      expectedHandSize = 2;
+
+      expect(player.numDice(), expectedHandSize);
+    });
+
+    test('Discarding to zero dice should mark player eliminated', () {
+      int expectedHandSize = 6;
+      Player player = getPlayer();
+      bool expectedEliminated = false;
+
+      expect(player.numDice(), expectedHandSize);
+      expect(player.eliminated, expectedEliminated);
+
+      player.discardDice(7);
+      expectedHandSize = 0;
+      expectedEliminated = true;
+
+      expect(player.numDice(), expectedHandSize);
+      expect(player.eliminated, expectedEliminated);
+    });
+  });
+}
