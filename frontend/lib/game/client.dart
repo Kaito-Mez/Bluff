7e31d06c46cf0f386f87997f55a936aeb208c40f
Kaito-Mez <jameskm721@gmail.com>
2024-02-02 20:43:10 +1100

    Initial Commit

--- frontend/lib/game/client.dart
+++ frontend/lib/game/client.dart
@@ -1,21 +1,23 @@
 import 'package:frontend/game/models/bet.dart';
-import 'package:frontend/game/models/events.dart';
 import 'package:frontend/game/models/dice.dart';
+import 'package:frontend/game/models/events.dart';
 import 'package:frontend/game/models/player.dart';
 import 'package:frontend/game/models/ruleset.dart';
+import 'package:smartlogger/smartlogger.dart';
 
 class Client {
   bool isAiClient = true;
-  int turnIndex = 0;
+  int clientID;
   Ruleset ruleset;
 
   EventsChannel netEvents;
   late EventsChannel uiEvents;
 
   late Bet bet;
+  late Player player;
   late List<Player> players;
 
-  Client(List<String> playerNames, this.ruleset, this.netEvents,
+  Client(this.clientID, List<String> playerNames, this.ruleset, this.netEvents,
       {EventsChannel? uiEvents}) {
     setupGame(playerNames);
 
@@ -35,22 +37,39 @@ class Client {
 
     players = List.generate(
         numPlayers,
-        (index) => Player(index, playerNames[index],
-            List.filled(numDice, Dice(numSides: numSides))));
+        (index) => Player(
+            index, playerNames[index], List.filled(numDice, Dice(numSides))));
+    player = players[clientID];
   }
 
   void onNetBet(BetEventArgs args) {
     bet = args.bet;
-    print("on bet");
 
-    if (!isAiClient) {
-      uiEvents.betEvent.broadcast(args);
+    if (!identical(args.sender, this)) {
+      if (!isAiClient) {
+        uiEvents.betEvent.broadcast(args);
+        uiEvents.turnEvent.broadcast(TurnEventArgs(getNextTurn(bet), this));
+      } else {
+        Log.v("TODO: AI.ACT ON BET");
+      }
     }
   }
 
   void onLocalBet(BetEventArgs args) {
     bet = args.bet;
+    if (identical(args.sender, this)) {
+      netEvents.betEvent.broadcast(args);
+    }
+  }
 
-    netEvents.betEvent.broadcast(args);
+  void onNetCall(CallEventArgs args) {
+    if (!identical(args.sender, this)) {
+      netEvents.revealHandEvent
+          .broadcast(RevealHandEventArgs(player.id, player.currentRoll, this));
+    }
+  }
+
+  int getNextTurn(Bet bet) {
+    return (bet.playerId + 1) % players.length;
   }
 }
