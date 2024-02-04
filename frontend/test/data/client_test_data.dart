dd10b3f3d92df7a31f1137c6783219edb7c771e2
Kaito-Mez <jameskm721@gmail.com>
2024-02-04 21:16:56 +1100

    Client test data created

--- frontend/test/data/client_test_data.dart
+++ frontend/test/data/client_test_data.dart
@@ -1,15 +1,34 @@
+import "package:flutter/services.dart";
 import "package:frontend/game/client.dart";
-import "network/events_channel_test_data.dart";
+import "package:frontend/game/models/ruleset.dart";
+import "package:frontend/game/network/client_events_channel.dart";
+import 'network/client_events_channel_test_data.dart';
 
 import "ruleset_test_data.dart";
 
 /// Generate a player client with default ruleset
 Client getPlayerClient() {
-  return Client(0, getRuleset(), getEventsChannel(),
-      uiEvents: getEventsChannel());
+  return Client(0, getRuleset(), getClientEventsChannel(),
+      uiEvents: getClientEventsChannel());
 }
 
 /// Generate an AI client with default ruleset
 Client getAiClient() {
-  return Client(0, getRuleset(), getEventsChannel());
+  return Client(0, getRuleset(), getClientEventsChannel());
+}
+
+/// Generate list of clients with one player and the rest being AI.
+List<Client> getClients(int quantity) {
+  ClientEventsChannel netEvents = getClientEventsChannel();
+  ClientEventsChannel uiEvents = getClientEventsChannel();
+  Ruleset ruleset = getRuleset();
+  List<Client> clients = List.empty(growable: true);
+
+  clients.add(Client(0, ruleset, netEvents, uiEvents: uiEvents));
+
+  for (var i = 1; i < quantity; i++) {
+    clients.add(Client(i, ruleset, netEvents));
+  }
+
+  return clients;
 }
--- frontend/test/data/network/events_channel_test_data.dart
+++
@@ -1,5 +0,0 @@
-import "package:frontend/game/network/events_channel.dart";
-
-EventsChannel getEventsChannel() {
-  return EventsChannel();
-}
