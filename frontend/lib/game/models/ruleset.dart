36bc264b3d7c4e5cb2e3e24c846e9bfd34481f1b
Kaito-Mez <jameskm721@gmail.com>
2024-02-05 11:40:03 +1100

    Added randomizeSeats and swapDirections to ruleset

--- frontend/lib/game/models/ruleset.dart
+++ frontend/lib/game/models/ruleset.dart
@@ -8,6 +8,12 @@ class Ruleset {
   ///Number of seconds a player has to make a bet before a random one is made
   int turnTime;
 
+  ///If players win in risky ways then they can heal dice
+  bool challenges;
+
+  ///Players can't lose their final dice if they were not the better or caller
+  bool guardianAngel;
+
   ///Players losing by a lot will be given extra info
   bool handicap;
 
@@ -17,11 +23,11 @@ class Ruleset {
   ///Lost dice are still rolled but no player can see them
   bool mysteryDice;
 
-  ///Players can't lose their final dice if they were not the better or caller
-  bool guardianAngel;
+  ///Order of players changes each round.
+  bool randomizeSeats;
 
-  ///If players win in risky ways then they can heal dice
-  bool challenges;
+  ///Swap direction of play each round.
+  bool swapDirection;
 
   ///Which sides of the dice are wildcards
   List<int> wildcards;
@@ -30,10 +36,12 @@ class Ruleset {
       {this.numDice = 30,
       this.numSides = 6,
       this.turnTime = -1,
+      this.challenges = false,
+      this.guardianAngel = true,
       this.handicap = false,
       this.heal = false,
       this.mysteryDice = false,
-      this.guardianAngel = true,
-      this.challenges = false,
+      this.randomizeSeats = false,
+      this.swapDirection = false,
       this.wildcards = const <int>[6]});
 }
