03206da920bb04b0bdb5b0cff3d5c6f73f2b29c6
Kaito-Mez <jameskm721@gmail.com>
2024-02-05 14:51:21 +1100

    Removed Frontend Backend Split in file structure

---
+++ lib/game/ai/ai_engine.dart
@@ -0,0 +1,30 @@
+import 'package:frontend/game/models/bet.dart';
+import 'package:frontend/game/models/player.dart';
+import 'package:frontend/game/models/ruleset.dart';
+
+class AiEngine {
+  Ruleset ruleset;
+  double difficulty;
+  double erraticness;
+
+  AiEngine(this.ruleset, this.difficulty, this.erraticness) {}
+
+  bool callBluff() {
+    bool callBluff = false;
+    return callBluff;
+  }
+
+  Bet makeBet(Player player, Bet lastBet) {
+    /// Some sort of assign probabilities to all potential bets then eliminate
+    /// all bets below some probability threshold based on difficulty then select
+    /// an option based on erraticness.
+    return lastBet;
+  }
+
+  List<Bet> getPossibleRaises() {
+    List<Bet> possibilites = List.empty(growable: true);
+    
+
+    return possibilites;
+  }
+}
