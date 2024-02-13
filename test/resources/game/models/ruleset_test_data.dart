cb067bd1c94ce6c0cdede3c1477cc70f6c2d0609
Kaito-Mez <jameskm721@gmail.com>
2024-02-13 15:05:34 +1100

    Added types to optional args

--- test/resources/game/models/ruleset_test_data.dart
+++ test/resources/game/models/ruleset_test_data.dart
@@ -2,17 +2,17 @@ import "package:frontend/game/models/ruleset.dart";
 
 /// Generates default ruleset with  30 and sides per dice 6.
 Ruleset getTestRuleset(
-    {numDice = 30,
-    numSides = 6,
-    turnTime = -1,
-    challenges = false,
-    guardianAngel = true,
-    handicap = false,
-    heal = false,
-    mysteryDice = false,
-    randomizeSeats = false,
-    swapDirection = false,
-    wildcards = const <int>[6]}) {
+    {int numDice = 30,
+    int numSides = 6,
+    int turnTime = -1,
+    bool challenges = false,
+    bool guardianAngel = true,
+    bool handicap = false,
+    bool heal = false,
+    bool mysteryDice = false,
+    bool randomizeSeats = false,
+    bool swapDirection = false,
+    List<int> wildcards = const <int>[6]}) {
   return Ruleset(
       numDice: numDice,
       numSides: numSides,
